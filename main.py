18b82b3b2053ac4a89cd4a6497775aeb64b845bf
David Walsh <davidtwalsh22@gmail.com>
2020-09-21 09:45:38 -0700

    init

---
+++ main.py
@@ -0,0 +1,46 @@
+import pygame
+
+pygame.init()
+
+# define colors
+BLACK = ( 0, 0, 0)
+WHITE = ( 255, 255, 255)
+GREEN = ( 0, 255, 0)
+RED = ( 255, 0, 0)
+
+
+# define window
+screenDimensions = (750, 500)
+screen = pygame.display.set_mode(screenDimensions)
+pygame.display.set_caption("425 Project")
+
+# define bool to decide when program ends
+isProgramRunning = True
+
+# clock controls framerate of program
+clock = pygame.time.Clock()
+fps = 60
+
+# main loop
+while isProgramRunning:
+    # main event loop
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            isProgramRunning = False
+    
+    # main logic here
+
+    # drawing here
+    screen.fill(WHITE) #clear screen to white
+
+    pygame.draw.rect(screen, RED , [55, 200, 100, 70], 0)
+    pygame.draw.line(screen,GREEN, [0, 0], [100, 100], 5)
+    pygame.draw.ellipse(screen, BLACK, [20, 20, 250, 100], 2)
+
+    #update screen and set framerate
+    pygame.display.flip()
+    clock.tick(fps)
+
+#end of main loop
+pygame.quit()
+
