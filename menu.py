96a5ac506fa97ef0bdfdd6efdcd451faa1a1608b
Jansen Dike <jansen@Jansens-MacBook-Pro.local>
2020-09-27 13:33:48 -0400

    added potential tutorial function and menu title color

--- menu.py
+++ menu.py
@@ -21,21 +21,26 @@ screen = pygame.display.set_mode(screenDimensions)
 pygame.display.set_caption("Main Menu - Fraction Visualization")
 
 def set_difficulty_level(value, difficulty):
-    # Do the job here !
+    # Do something here
     pass
 
 def start_the_game():
-    # Do the job here !
+    # Do something here
+    pass
+
+def tutorial():
+    # Do something here
     pass
 
 
+
 # Creating a custom theme based off SOLARIZED
-copy = pygame_menu.themes.THEME_SOLARIZED.copy
+# copy = pygame_menu.themes.THEME_SOLARIZED.copy
 
 # Creating a custom theme from scratch
 fraction_theme = pygame_menu.themes.Theme(background_color=(0, 0, 0, 0), # transparent background
                 title_shadow=True,
-                title_background_color=(4, 47, 126)
+                title_background_color=GREEN
                 )
 
 # Adding image to be used as background
@@ -68,6 +73,7 @@ menu.add_text_input('Name : ', default='Olive Math')
 menu.add_selector('Difficulty Level :  ', [('Easy', 1), ('Medium', 2), ('Hard', 3)], onchange=set_difficulty_level)
 
 # Start and quit events
+menu.add_button('Tutorial', tutorial)
 menu.add_button('Start', start_the_game)
 menu.add_button('Quit', pygame_menu.events.EXIT)
 
