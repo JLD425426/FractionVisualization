507f1649ad9db77d7dd18c9f8f578a048c24e047
Jansen Dike <jansen@Jansens-MacBook-Pro.local>
2020-10-04 15:32:43 -0400

    added pop up quit message and background

--- mainWithMenu.py
+++ mainWithMenu.py
@@ -25,6 +25,9 @@ isProgramRunning = True
 clock = pygame.time.Clock()
 fps = 60
 
+# Load image in for background
+background_img = pygame.image.load("yellow_background.jpg")
+
 # create drawable object lists
 rectangles = list()
 cutMarkers = list()
@@ -36,6 +39,7 @@ testRectangle.createCutMarkers(3,4)
 # Create fonts for interface
 title_font = pygame.font.SysFont(None, 60)
 button_font = pygame.font.SysFont(None, 25)
+message_font = pygame.font.SysFont(None, 32)
 
 # Function to create and draw text object
 def draw_text(text, font, color, surface, x, y):
@@ -52,8 +56,9 @@ def main_menu():
     while True:
  
         screen.fill((255, 245, 112))        # Fill background
+        screen.blit(background_img, (0, 100))
         m1x, m1y = pygame.mouse.get_pos()   # Get mouse position
-        title_bar = pygame.Rect(0, 0, 700, 100)
+        title_bar = pygame.Rect(0, 0, 695, 100)
 
         # Create start and quit buttons with rect
         start_button = pygame.Rect(int((WIDTH/2))-100, int(HEIGHT/4), 200, 50)
@@ -65,10 +70,11 @@ def main_menu():
                 main_prog()
         if quit_button.collidepoint((m1x, m1y)):    # Quits game on quit button click
             if click:
-                pygame.quit()
+                quit_message()
         
         # Drawing the buttons and text for menu
         pygame.draw.rect(screen, (245, 222, 47), title_bar)
+        pygame.draw.rect(screen, (0, 0, 0), title_bar, 7)
         draw_text('Main Menu', title_font, (8, 41, 255), screen, int(WIDTH/2), int(HEIGHT/12))
         pygame.draw.rect(screen, (8, 41, 255), start_button)
         draw_text('Start', button_font, (0,0,0), screen, WIDTH/2, int((HEIGHT/4)+25))
@@ -89,7 +95,51 @@ def main_menu():
         pygame.display.update()
         clock.tick(60)
  
+# Displays "are you sure you want to quit" message and gets click response
+def quit_message():
+    click = False
+    m1x = 0      # Get error if you don't set value for mx and my here
+    m1y = 0      # Maybe pass as parameter for main_prog()
+    while True:
+ 
+        m1x, m1y = pygame.mouse.get_pos()   # Get mouse position
 
+        # Create pop up window
+        pop_up = pygame.Rect(100, 50, 500, 500)
+
+        # Create yes and no buttons with rect
+        yes_button = pygame.Rect(int((WIDTH/2))-100, int(HEIGHT/3), 200, 50)
+        no_button = pygame.Rect(int((WIDTH/2))-100, int(HEIGHT/2), 200, 50)
+
+        # Check if mouse is on a button when clicked
+        if no_button.collidepoint((m1x, m1y)):   # Calls main program if start is selected
+            if click:
+                main_menu()
+        if yes_button.collidepoint((m1x, m1y)):    # Quits game on quit button click
+            if click:
+                pygame.quit()
+        
+        # Drawing the buttons and text for pop-up
+        pygame.draw.rect(screen, (255, 255, 255), pop_up)
+        draw_text('Are you sure you would like to quit?', message_font, (0,0,0), screen, WIDTH/2, HEIGHT/5)
+        pygame.draw.rect(screen, (8, 41, 255), yes_button)
+        draw_text('Yes', button_font, (0,0,0), screen, WIDTH/2, int((HEIGHT/3)+25))
+        pygame.draw.rect(screen, (8, 41, 255), no_button)
+        draw_text('No', button_font, (0,0,0), screen, WIDTH/2, int((HEIGHT/2)+25))
+
+        # Main event loop
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+            if event.type == KEYDOWN:
+                if event.key == K_ESCAPE:
+                    pygame.quit()
+            if event.type == MOUSEBUTTONDOWN:
+                if event.button == 1:
+                    click = True
+ 
+        pygame.display.update()
+        clock.tick(60)
 
 def main_prog():
     mx = 0      # Get error if you don't set value for mx and my here
